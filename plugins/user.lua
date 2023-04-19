6a20dcb7a8ec245c64b57089e7302f07dc238177
Andy Tran <transient.tran@gmail.com>
2023-04-18 21:29:11 -0400

    added catppuccin plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,13 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "catppuccin/nvim",
+    as = "catppuccin",
+    config = function()
+      require("catppuccin").setup {
+        flavour = "frappe"
+      }
+    end,
+  }
 }
